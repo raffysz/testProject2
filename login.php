df8ef338f1088a87bd8b3f55631a159779684c2a
raffy <raffysz@outlook.com>
2016-04-27 21:16:57 +0100

    Commit

--- login.php
+++ login.php
@@ -10,30 +10,36 @@ if(isset($_POST["submit"]))
 		$error = "Both fields are required.";
 	}else
 	{
-		$user = $_POST[ 'username' ];
-		$user = stripslashes( $user );
-		$user = mysqli_real_escape_string( $user );
+		// Define $username and $password
+		$username=$_POST['username'];
+		$username=mysqli_real_escape_string( $username );
 
-		$pass = $_POST[ 'password' ];
-		$pass = stripslashes( $pass );
-		$pass = mysqli_real_escape_string( $pass );
+		$password=$_POST['password'];
+		$password=mysqli_real_escape_string( $passworde );
 
-		$query  = "SELECT * FROM `users` WHERE username = '$user' AND password = '$pass';";
-		$result = mysqli_query( $query ) or die( '<pre>' . mysqli_error() . '</pre>' );
 
-		if(mysqli_num_rows($result) == 1) {
-			$_SESSION['username'] = $user; // Initializing Session
+
+		//Check username and password from database
+		$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
+		$result=mysqli_query($db,$sql);
+		$row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
+
+		//If username and password exist in our database then create a session.
+		//Otherwise echo error.
+
+		if(mysqli_num_rows($result) == 1)
+		{
+			$_SESSION['username'] = $username; // Initializing Session
 			header("location: photos.php"); // Redirecting To Other Page
-		}
-		else {
-			// Login failed
+		}else
+		{
 			sleep( rand( 0, 3 ) );
-			$error =  "<pre><br />Username and/or password incorrect.</pre>";
+			$error = "Incorrect username or password.";
 		}
 
 		mysqli_close();
-	}
 
+	}
 }
 
 ?>
