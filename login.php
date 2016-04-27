b200e49f1095d4ba81565ea844abb7804f60ddec
raffy <raffysz@outlook.com>
2016-04-27 19:36:04 +0100

    Commit

--- login.php
+++ login.php
@@ -1,39 +1,39 @@
+
 <?php
-session_start();
-include("connection.php"); //Establishing connection with our database
 
-$error = ""; //Variable for storing our errors.
-if(isset($_POST["submit"]))
-{
-	if(empty($_POST["username"]) || empty($_POST["password"]))
-	{
-		$error = "Both fields are required.";
-	}else
-	{
-		// Define $username and $password
-		$username=$_POST['username'];
-		$password=$_POST['password'];
+if( isset( $_GET[ 'submit' ] ) ) {
+	// Check Anti-CSRF token
+	checkToken( $_REQUEST[ 'user_token' ], $_SESSION[ 'session_token' ], 'index.php' );
 
+	// Sanitise username input
+	$user = $_GET[ 'username' ];
+	$user = stripslashes( $user );
+	$user = mysql_real_escape_string( $user );
 
+	// Sanitise password input
+	$pass = $_GET[ 'password' ];
+	$pass = stripslashes( $pass );
+	$pass = mysql_real_escape_string( $pass );
+	$pass = md5( $pass );
 
-		//Check username and password from database
-		$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
-		$result=mysqli_query($db,$sql);
-		$row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
+	// Check database
+	$query  = "SELECT * FROM `users` WHERE username = '$user' AND password = '$pass';";
+	$result = mysql_query( $query ) or die( '<pre>' . mysql_error() . '</pre>' );
 
-		//If username and password exist in our database then create a session.
-		//Otherwise echo error.
-
-		if(mysqli_num_rows($result) == 1)
-		{
-			$_SESSION['username'] = $username; // Initializing Session
-			header("location: photos.php"); // Redirecting To Other Page
-		}else
-		{
-			$error = "Incorrect username or password.";
-		}
-
+	if( $result && mysql_num_rows( $result ) == 1 ) {
+		$_SESSION['username'] = $username; // Initializing Session
+		header("location: photos.php"); // Redirecting To Other Page
 	}
+	else {
+		// Login failed
+		sleep( rand( 0, 3 ) );
+		$error = "Incorrect username or password.";
+	}
+
+	mysql_close();
 }
+
+// Generate Anti-CSRF token
+generateSessionToken();
 
 ?>
