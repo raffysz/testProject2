a400a4b48cb07daf788de12b29ff44cff75457db
raffy <raffysz@outlook.com>
2016-04-27 20:37:28 +0100

    Commit

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER', 'eu-cdbr-azure-north-d.cloudapp.net');
-define('DB_USERNAME', 'b99256e599d9fc');
-define('DB_PASSWORD', '64c23a04');
-define('DB_DATABASE', 'rgutest');
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME', 'b06d6b8eb1aeba');
+define('DB_PASSWORD', 'b4f54932');
+define('DB_DATABASE', 'testProject2');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 ?>
