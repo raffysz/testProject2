f1fd3e169e5af671947b04a1f35273cd9f40a3c6
raffy <raffysz@outlook.com>
2016-04-30 13:25:23 +0100

    Commit

--- search.php
+++ search.php
@@ -3,6 +3,7 @@ $resultText = "";
 if(isset($_POST["submit"]))
 {
     $name = $_POST["username"];
+    $name = mysqli_real_escape_string ($db, $name);
 
     $sql="SELECT userID FROM users WHERE username='$name'";
     $result=mysqli_query($db,$sql);
