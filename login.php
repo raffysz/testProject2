581b4d8a7f46ee68ee47f3202a6dd6cffa8b8784
raffysz <raffysz@outlook.com>
2016-05-01 22:14:42 +0100

    commit

--- login.php
+++ login.php
@@ -19,7 +19,24 @@ if(isset($_POST["submit"]))
 		$password = mysqli_real_escape_string($db, $password);
 		$password = htmlspecialchars($password);
 
+		$total_failed_login =3;
+		$lockout_time =15;
+		$account_locked =false;
 
+		$sql="SELECT filed_login, last_login FROM users WHERE username='$username'";
+		$result=mysqli_query($db,$sql);
+		$row=mysqli_fetch_array($result,MYSQLI_ASSOC) ;
+
+		if((mysqli_num_rows($result) == 1) && ($row['failed_login'] >=$total_failed_login){
+			$error = "This account has been locked out due to too many incorrect logins";
+
+			$last_login = $row['last_login'];
+			$last_login = strtotime("{$last_login}+{$lockout_time} minutes");
+			$timenow = strtotime("now");
+
+			if($timenow > $timeout)
+				$account_locked = true;
+		}
 
 		//Check username and password from database
 		$sql="SELECT userID FROM users WHERE username='$username' and password='$password'";
@@ -29,14 +46,24 @@ if(isset($_POST["submit"]))
 		//If username and password exist in our database then create a session.
 		//Otherwise echo error.
 
-		if(mysqli_num_rows($result) == 1)
+		if((mysqli_num_rows($result) == 1)&&($account_locked == false))
 		{
 			$_SESSION['username'] = $username; // Initializing Session
+
+			$sql="UPDATE users SET failed_login = '0' WHERE username = '$username";
+
 			header("location: photos.php"); // Redirecting To Other Page
+
 		}else
 		{
-			$error = "Incorrect username or password.";
+			sleep(rand( 2, 5));
+			$error = "Incorrect username or password, or account locked because of too many failed logins.";
+			$msg = "If this is the case, please try again in {$lockout_time} minutes";
+
+			$sql="UPDATE users SET failed_login = (failed_login + 1) WHERE username = '$username";
 		}
+
+		$sql="UPDATE users SET last_login = NOW() WHERE name = '$username";
 
 	}
 }
