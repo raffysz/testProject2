0f1b7101e00c31e8b5104c6b4c43c36bb408f157
raffy <raffysz@outlook.com>
2016-05-01 19:55:52 +0100

    Commit

--- photo.php
+++ photo.php
@@ -15,6 +15,14 @@
 <div id="photo">
     <?php
 
+    function xssafe($data, $encoding='UTF-8'){
+        return htmlspecialchars($data, ENT_QUOTES|ENT_HTML401,$encoding);
+    }
+
+    function xecho($data){
+        echo xssafe($data);
+    }
+
         if(isset($_GET['id'])){
             $photoID = $_GET['id'];
             $photoSql="SELECT * FROM photos WHERE photoID='$photoID'";
