8222a94724e0ca908b77f141104e08cfa0823d92
raffy <raffysz@outlook.com>
2016-05-01 19:10:01 +0100

    Commit

--- photo.php
+++ photo.php
@@ -28,14 +28,13 @@
 
                 $commentSql="SELECT * FROM comments WHERE photoID='$photoID'";
                 $commentresult=mysqli_query($db,$commentSql) or die(mysqli_error($db));
-                if(mysqli_num_rows($commentresult)>=1) {
+                if(mysqli_num_rows($commentresult)>1) {
                     $commentRow=mysqli_fetch_assoc($commentresult);
                     echo "<h2> Comments </h2>";
                         echo "<div class = 'comments'>";
-                        echo "<h3>".$commentRow['postDate']."</h3>";
-                        echo "<p>".$commentRow['description']."</p>";
+                        echo "<h3>" . $commentRow['postDate'] . "</h3>";
+                        echo "<p>" . $commentRow['description'] . "</p>";
                         echo "</div>";
-
                 }
                 echo "<a href='addcommentform.php?id=".$photoID."'> Add Comment</a><br>";
 
