8efeea56fdf860a8a4b82942abe6bee59ad81495
raffy <raffysz@outlook.com>
2016-05-01 18:29:15 +0100

    Commit

--- addphoto.php
+++ addphoto.php
@@ -23,7 +23,7 @@ if(isset($_POST["submit"]))
 
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false){
-        $msg = "File is an image -" .$check["mime"]. ".";
+        $errors = "File is an image -" .$check["mime"]. ".";
         $uploadOk = 1;
     }else{
         $errors = "File is not an image.";
